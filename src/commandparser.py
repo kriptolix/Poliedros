e599b94fcb173323df44fbd8c80b8fc7f1b9b212
Kripto Lix <kriptolix@gmail.com>
2025-05-14 13:45:06 -0300

    Ok, onme more try with pyparsing before give up

--- src/commandparser.py
+++ src/commandparser.py
@@ -1,95 +1,43 @@
 from pyparsing import (
-    Word, Literal, Group, Optional, Forward, Suppress, ParserElement, nums,
-    oneOf, ZeroOrMore, delimitedList
+    Word, Literal, Group, Optional, Suppress, alphas,
+    oneOf, ZeroOrMore 
 )
 
 
-def command_parser(command):
+def parse_expression(command):
 
-    ParserElement.enablePackrat()
+    func_id = Word(alphas, min=1, max=2)
 
-    # integer = Word(nums)
-
-    integer = Word("123456789", "0123456789", min=1, max=3).setParseAction(
+    func_number = Word("123456789", "0123456789", min=1, max=3).setParseAction(
         lambda tokens: int(tokens[0]))
-    plus = Literal('+')
-    minus = Literal('-')
 
-    range_op = Literal("..")
-    range_expr = integer("start") + range_op + integer("end")
-
-    number_or_range = range_expr | integer
-
-    dice_notation = Group(
-        Optional(integer, default="1")("count") +
+    dice_expr = Group(
+        Optional(func_number, default="1")("count") +
         Literal("d") +
-        (integer | Literal("f"))("faces")
+        (func_number | Literal("f"))("faces")
     )
 
-    expr = Forward()
+    plus = Literal("+")
+    minus = Literal("-")
+    pipe = Suppress("|")
 
-    parenthesized_expr = (Suppress("(") + expr + Suppress(")"))
+    op = plus | minus | pipe
 
-    integer_expr = integer.copy()
+    comparator = oneOf("> <")
+    comp_expr = Group(comparator + func_number)
 
-    multiroll = Group(
-        Literal("multiroll")("command") +
-        Group(number_or_range)("amount") +
-        dice_notation("dice")
-    )
+    range_expr = Group(func_number + Literal("..") + func_number)
 
-    highest = Group(
-        (Literal("highest") | Literal("h"))("command") +
-        Group(Optional(delimitedList(number_or_range, delim=","), default="1"))("amount") +
-        Suppress("in") +
-        (parenthesized_expr | dice_notation | integer_expr)("dice")
-    )
+    func_param = range_expr | comp_expr | func_number
 
-    lowest = Group(
-        Literal("lowest")("command") +
-        Group(Optional(delimitedList(number_or_range, delim=","), default="1"))("amount") +
-        Suppress("in") +
-        (parenthesized_expr | dice_notation | integer_expr)("dice")
-    )
+    func_expr = Group(func_id + Suppress(":") + func_param)
 
-    count = Group(
-        Literal("count")("command") +
-        (
-            Group(oneOf("< >")("comparator") + number_or_range("amount")) |
-            Group(delimitedList(number_or_range, delim=",")("values")) |
-            number_or_range("amount")
-        ) +
-        Suppress("in") +
-        (parenthesized_expr | dice_notation | integer_expr)("dice")
-    )
+    operand = dice_expr | func_expr | func_number
 
-    explode = Group(
-        Literal("explode")("command") +
-        Group(Optional(delimitedList(number_or_range, delim=",")))("values") +
-        Suppress("in") +
-        (parenthesized_expr | dice_notation | integer_expr)("dice")
-    )
-
-    reroll = Group(
-        Literal("reroll")("command") +
-        (
-            Group(oneOf("< >")("comparator") + number_or_range("amount")) |
-            Group(delimitedList(number_or_range, delim=",")("values")) |
-            number_or_range("amount")
-        ) +
-        Suppress("in") +
-        (parenthesized_expr | dice_notation | integer_expr)("dice")
-    )
-
-    extended_operand = (
-        parenthesized_expr | highest | lowest | count | 
-        explode | reroll | multiroll | dice_notation | integer_expr
-    )
-
-    expr <<= extended_operand + ZeroOrMore((plus | minus) + extended_operand)
+    expression = operand + ZeroOrMore(op + operand)
 
     try:
-        result = expr.parseString(command, parseAll=True)
+        result = expression.parseString(command)
         return result
     except Exception as e:
         print(f"Erro ao interpretar '{command}': {e}")
--- src/tests.py
+++ src/tests.py
@@ -1,39 +1,22 @@
-from .commandparser import command_parser
+from .commandparser import parse_expression
 from .executor import split_elements
 
-tests = ["count 5,6 in (highest 2 in 5d6)"]
-
-tests2 = [
-    "explode 1 in 4d6", "4d6|h:2|c:5,6"
-    "explode 1 in 4d6",
-    "explode 5,6 in 4d6",
-    "explode 3..5 in 4d10",
-    "explode in 4d6",
-    "5d6",
-    "d10",
-    "highest 2 in 4d10",
-    "highest in 3d8",
-    "lowest 3 in 6d12",
-    "lowest in 4d10",
-    "count 6 in 5d6",
-    "count 3,4 in 6d8",
-    "count 2..4 in 8d10",
-    "reroll < 3 in 4d12",
-    "reroll > 4 in 5d10",
-    "reroll 3..5 in 4d12",
-    "3d6+3",
-    "(explode in 4d10)-1",
-    "highest 2 in 6d10 + 5",
-    "3d6+2-1",
-    "(3d6)+5",
-    "count 5,6 in (highest 2 in 5d6)",
-    "3d6+(count 6 in 3d6)"
+tests = [
+    ("5d6+1d4|h:3..5|c:>4",
+     [[5, 'd', 6], '+', [1, 'd', 4], ['h', [3, '..', 5]], ['c', ['>', 4]]]),
+    ("4d6 | h:2 + 1",
+     [[4, 'd', 6], ['h', [2]], '+', 1]),
+    ("d10",
+     [[1, 'd', 10]]),
+    ("3df",
+     [[3, 'd', 'f']])
 ]
 
-def run_tests():  # 5d6:h2:c5,6
-    expression = "count 5,6 in (highest 2 in 5d6)"
-    result = command_parser(expression)
+
+def run_tests():  # "4d6 + 1 | h:2 | c:5,6" 3d6|c:2
+    expression = "5d6+1d4|h:3..5|c:>4"
+    result = parse_expression(expression)
     print(f"{expression} -> {result}")
-    # total  
-    total, log = split_elements(result)
-    print(f"total: {total}, log :{log}")
+    # total
+    # total, log = split_elements(result)
+    # print(f"total: {total}, log :{log}")
