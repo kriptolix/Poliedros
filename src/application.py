3c46deb93692cedc7380a6a08cb5a68bec9cb519
Kriptolix <kriptolix@gmail.com>
2025-10-31 08:33:55 -0300

    Last commit for 1.5.2

--- src/application.py
+++ src/application.py
@@ -80,7 +80,7 @@ class PoliedrosApplication(Adw.Application):
         about = Adw.AboutDialog.new_from_appdata(
             '/io/github/kriptolix/Poliedros'
             '/data/io.github.kriptolix.Poliedros.metainfo.xml.in',
-            '1.5.0'
+            '1.5.2'
         )
 
         about.set_translator_credits(_('translator_credits'))
